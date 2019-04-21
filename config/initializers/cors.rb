2208cda325545e8e026817ce27fd1426887f24a6
rchow1944 <rchow2@buffalo.edu>
2019-04-21 16:55:35 -0400

    added heroku frontend origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'https://rchow-twips.herokuapp.com']
+    origins ['http://localhost:3000', 'https://rchow-twips.herokuapp.com', 'https://twips-frontend.herokuapp.com']
 
     resource '*',
       headers: :any,
